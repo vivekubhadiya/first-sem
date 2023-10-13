b17f7deffb32dbf360cb981e1edbc4776b55cb8d
vivekubhadiya <143770316+vivekubhadiya@users.noreply.github.com>
2023-10-13 09:33:45 +0530

    Add files via upload

---
+++ printestimatethevalue.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+int i,n  ,fac=1;
+float ans, sum=1.0;
+printf("enter your number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+
+		fac=fac*i; 
+	
+}
+pri
+ans=1/fac;
+sum=sum+ans;
+
+printf("%f",sum);
+}
+
+
+
+
+
